897a9188c7a44360f166505ac6f9d13c99d91ae8
dakattack <dakotawells96@gmail.com>
2020-05-27 17:45:47 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 # bot.py
 import os
 import json
-
 import discord
+import urllib
+
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 class MyClient(discord.Client):
+	DBM_URL = "https://addons-ecs.forgesvc.net/api/v2/addon/3358"
+	DBM_JSON = urllib.urlopen(DBM_URL)
+	DBM_Parse = json.loads(DBM_JSON)
+	print(DBM_Parse)
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
