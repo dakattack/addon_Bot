eeba00a615e7bbfb98282e8a87e152dd019725bd
Dakota Wells <dakotawells96@gmail.com>
2020-05-27 17:54:05 -0400

    read dbm json

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 import os
 import json
 import discord
-import urllib
+import urllib.request
 
 from dotenv import load_dotenv
 
@@ -11,14 +11,15 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 class MyClient(discord.Client):
 	DBM_URL = "https://addons-ecs.forgesvc.net/api/v2/addon/3358"
-	DBM_JSON = urllib.urlopen(DBM_URL)
-	DBM_Parse = json.loads(DBM_JSON)
+	with urllib.request.urlopen(DBM_URL) as url:
+		DBM_JSON = url.read()
+		DBM_Parse = json.loads(DBM_JSON)
 	print(DBM_Parse)
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+	async def on_ready(self):
+		print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+	async def on_message(self, message):
+		print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
 
